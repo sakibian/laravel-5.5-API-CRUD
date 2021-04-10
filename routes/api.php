2f346c6754284083b2f910e07c0247636a60de04
Sakib <sakib192@gmail.com>
2021-04-10 11:50:32 +0600

    Full CRUD implemented with Vue.js

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+}); 
 
 // List articles
 Route::get('articles', 'ArticleController@index');
